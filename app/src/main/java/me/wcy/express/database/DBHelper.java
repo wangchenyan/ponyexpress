1fa5611581e5e6ff10cfd4e47327f6dc959aa171
wcy <919247701@qq.com>
2015-09-20 16:24:02 +0800

    update database version

--- app/src/main/java/me/wcy/express/database/DBHelper.java
+++ app/src/main/java/me/wcy/express/database/DBHelper.java
@@ -1,7 +1,5 @@
 package me.wcy.express.database;
 
-import java.sql.SQLException;
-
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -9,33 +7,34 @@ import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 
+import java.sql.SQLException;
+
 public class DBHelper extends OrmLiteSqliteOpenHelper {
-	private static final String DATABASE_NAME = "express";
-	private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "express";
+    private static final int DATABASE_VERSION = 2;
 
-	public DBHelper(Context context) {
-		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-	}
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
-	@Override
-	public void onCreate(SQLiteDatabase arg0, ConnectionSource arg1) {
-		try {
-			TableUtils.createTableIfNotExists(connectionSource, History.class);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void onCreate(SQLiteDatabase arg0, ConnectionSource arg1) {
+        try {
+            TableUtils.createTableIfNotExists(connectionSource, History.class);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	public void onUpgrade(SQLiteDatabase arg0, ConnectionSource arg1, int arg2,
-			int arg3) {
-		try {
-			TableUtils.dropTable(connectionSource, History.class, true);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, ConnectionSource arg1, int arg2, int arg3) {
+        try {
+            TableUtils.dropTable(connectionSource, History.class, true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-		onCreate(arg0, arg1);
-	}
+        onCreate(arg0, arg1);
+    }
 
 }
