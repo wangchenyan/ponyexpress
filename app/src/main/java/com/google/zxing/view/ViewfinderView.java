f137e188768801e22a65a09fa6b2a5cf5b03a515
hzwangchenyan <hzwangchenyan@corp.netease.com>
2016-01-19 16:39:39 +0800

    add scan animation

--- app/src/main/java/com/google/zxing/view/ViewfinderView.java
+++ app/src/main/java/com/google/zxing/view/ViewfinderView.java
@@ -17,9 +17,12 @@
 package com.google.zxing.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.PixelFormat;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -34,29 +37,41 @@ import me.wcy.express.R;
  * animation and result points.
  */
 public final class ViewfinderView extends View {
-    private static final int[] SCANNER_ALPHA = {0, 64, 128, 192, 255, 192, 128, 64};
-    private static final long ANIMATION_DELAY = 100L;
-    private final int mMaskColor;
-    private final int mFrameColor;
-    private final int mLaserColor;
-    private int mScannerAlpha;
+    private static final long ANIMATION_DELAY = 50L;
+    private static int BORDER_WIDTH;
+    private static int BORDER_HEIGHT;
+    private static int LASER_HEIGHT;
+    private static int LASER_PADDING;
+    private static int LASER_BASE_OFFSET;
     private Paint mFinderMaskPaint;
     private Paint mBorderPaint;
     private Paint mLaserPaint;
+    private final int mMaskColor;
+    private final int mFrameColor;
+    private final int mLaserColor;
+    private Bitmap mLaserLine;
+    private int mLaserOffset = 0;
 
     // This constructor is used when the class is built from an XML resource.
     public ViewfinderView(Context context, AttributeSet attrs) {
         super(context, attrs);
         // Initialize these once for performance rather than calling them every time in onDraw().
+        BORDER_WIDTH = dp2px(16);
+        BORDER_HEIGHT = dp2px(2);
+        LASER_HEIGHT = dp2px(2);
+        LASER_PADDING = dp2px(16);
+        LASER_BASE_OFFSET = dp2px(1);
+
+        mFinderMaskPaint = new Paint();
+        mBorderPaint = new Paint();
+        mLaserPaint = new Paint();
         mMaskColor = getResources().getColor(R.color.viewfinder_mask);
         mFrameColor = getResources().getColor(R.color.viewfinder_frame);
         mLaserColor = getResources().getColor(R.color.viewfinder_laser);
-        mScannerAlpha = 0;
-        mFinderMaskPaint = new Paint();
+
         mFinderMaskPaint.setColor(mMaskColor);
-        mBorderPaint = new Paint();
         mBorderPaint.setColor(mFrameColor);
-        mLaserPaint = new Paint();
+        mBorderPaint.setStrokeWidth(BORDER_HEIGHT);
         mLaserPaint.setColor(mLaserColor);
     }
 
@@ -91,27 +106,36 @@ public final class ViewfinderView extends View {
     }
 
     private void drawViewFinderBorder(Canvas canvas, Rect frame) {
-        int strokeWidth = dp2px(2);
-        int borderLength = dp2px(16);
-        int offset = strokeWidth / 2;
-        mBorderPaint.setStrokeWidth(strokeWidth);
-        canvas.drawLine((float) (frame.left + offset), (float) (frame.top), (float) (frame.left + offset), (float) (frame.top + borderLength), mBorderPaint);
-        canvas.drawLine((float) (frame.left), (float) (frame.top + offset), (float) (frame.left + borderLength), (float) (frame.top + offset), mBorderPaint);
-        canvas.drawLine((float) (frame.left + offset), (float) (frame.bottom), (float) (frame.left + offset), (float) (frame.bottom - borderLength), mBorderPaint);
-        canvas.drawLine((float) (frame.left), (float) (frame.bottom - offset), (float) (frame.left + borderLength), (float) (frame.bottom - offset), mBorderPaint);
-        canvas.drawLine((float) (frame.right - offset), (float) (frame.top), (float) (frame.right - offset), (float) (frame.top + borderLength), mBorderPaint);
-        canvas.drawLine((float) (frame.right), (float) (frame.top + offset), (float) (frame.right - borderLength), (float) (frame.top + offset), mBorderPaint);
-        canvas.drawLine((float) (frame.right - offset), (float) (frame.bottom), (float) (frame.right - offset), (float) (frame.bottom - borderLength), mBorderPaint);
-        canvas.drawLine((float) (frame.right), (float) (frame.bottom - offset), (float) (frame.right - borderLength), (float) (frame.bottom - offset), mBorderPaint);
+        int halfStroke = BORDER_HEIGHT / 2;
+        canvas.drawLine((float) (frame.left + halfStroke), (float) (frame.top), (float) (frame.left + halfStroke), (float) (frame.top + BORDER_WIDTH), mBorderPaint);
+        canvas.drawLine((float) (frame.left), (float) (frame.top + halfStroke), (float) (frame.left + BORDER_WIDTH), (float) (frame.top + halfStroke), mBorderPaint);
+        canvas.drawLine((float) (frame.left + halfStroke), (float) (frame.bottom), (float) (frame.left + halfStroke), (float) (frame.bottom - BORDER_WIDTH), mBorderPaint);
+        canvas.drawLine((float) (frame.left), (float) (frame.bottom - halfStroke), (float) (frame.left + BORDER_WIDTH), (float) (frame.bottom - halfStroke), mBorderPaint);
+        canvas.drawLine((float) (frame.right - halfStroke), (float) (frame.top), (float) (frame.right - halfStroke), (float) (frame.top + BORDER_WIDTH), mBorderPaint);
+        canvas.drawLine((float) (frame.right), (float) (frame.top + halfStroke), (float) (frame.right - BORDER_WIDTH), (float) (frame.top + halfStroke), mBorderPaint);
+        canvas.drawLine((float) (frame.right - halfStroke), (float) (frame.bottom), (float) (frame.right - halfStroke), (float) (frame.bottom - BORDER_WIDTH), mBorderPaint);
+        canvas.drawLine((float) (frame.right), (float) (frame.bottom - halfStroke), (float) (frame.right - BORDER_WIDTH), (float) (frame.bottom - halfStroke), mBorderPaint);
     }
 
     private void drawLaser(Canvas canvas, Rect frame) {
-        mLaserPaint.setAlpha(SCANNER_ALPHA[mScannerAlpha]);
-        mScannerAlpha = (mScannerAlpha + 1) % SCANNER_ALPHA.length;
-        int middle = frame.height() / 2 + frame.top;
-        int offsetX = dp2px(2);
-        int offsetY = dp2px(0.5f);
-        canvas.drawRect(frame.left + offsetX, middle - offsetY, frame.right - offsetX, middle + offsetY, mLaserPaint);
+        if (mLaserOffset >= frame.height() - LASER_HEIGHT) {
+            mLaserOffset = 0;
+        }
+        if (mLaserLine == null) {
+            Drawable drawable = getResources().getDrawable(R.drawable.ic_scan_line);
+            int width = frame.width() - LASER_PADDING * 2;
+            mLaserLine = drawable2Bitmap(drawable, width, LASER_HEIGHT);
+        }
+        canvas.drawBitmap(mLaserLine, frame.left + LASER_PADDING, frame.top + mLaserOffset, mLaserPaint);
+        mLaserOffset += LASER_BASE_OFFSET;
+    }
+
+    private Bitmap drawable2Bitmap(Drawable drawable, int width, int height) {
+        Bitmap bitmap = Bitmap.createBitmap(width, height, drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888 : Bitmap.Config.RGB_565);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, width, height);
+        drawable.draw(canvas);
+        return bitmap;
     }
 
     private int dp2px(float dpValue) {
