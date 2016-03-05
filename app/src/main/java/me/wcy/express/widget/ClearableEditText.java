f6dc2a5593ef6f1316fa015d05013a14f62b5251
wcy <919247701@qq.com>
2016-03-05 21:54:30 +0800

    bug fix

--- app/src/main/java/me/wcy/express/widget/ClearableEditText.java
+++ app/src/main/java/me/wcy/express/widget/ClearableEditText.java
@@ -14,7 +14,11 @@ import me.wcy.express.R;
  * Created by hzwangchenyan on 2016/3/5.
  */
 public class ClearableEditText extends AppCompatEditText {
-    private Drawable mDrawable;
+    private static final int DRAWABLE_LEFT = 0;
+    private static final int DRAWABLE_TOP = 1;
+    private static final int DRAWABLE_RIGHT = 2;
+    private static final int DRAWABLE_BOTTOM = 3;
+    private Drawable mClearDrawable;
 
     public ClearableEditText(Context context) {
         super(context);
@@ -32,38 +36,38 @@ public class ClearableEditText extends AppCompatEditText {
     }
 
     private void init() {
-        mDrawable = getResources().getDrawable(R.drawable.ic_edit_text_clear);
-        assert mDrawable != null;
-        mDrawable.setBounds(0, 0, mDrawable.getMinimumWidth(), mDrawable.getMinimumHeight());
+        mClearDrawable = getResources().getDrawable(R.drawable.ic_edit_text_clear);
+        assert mClearDrawable != null;
+        mClearDrawable.setBounds(0, 0, mClearDrawable.getIntrinsicWidth(), mClearDrawable.getIntrinsicHeight());
     }
 
     @Override
     protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
         super.onTextChanged(text, start, lengthBefore, lengthAfter);
-        showClearIcon(hasFocus() && text.length() > 0);
+        setClearIconVisible(hasFocus() && text.length() > 0);
     }
 
     @Override
     protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
         super.onFocusChanged(focused, direction, previouslyFocusedRect);
-        showClearIcon(focused && length() > 0);
+        setClearIconVisible(focused && length() > 0);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_UP:
-                Drawable drawable = getCompoundDrawables()[2];
+                Drawable drawable = getCompoundDrawables()[DRAWABLE_RIGHT];
                 if (drawable != null && event.getRawX() >= (getRight() - drawable.getBounds().width())) {
                     setText("");
-                    return true;
                 }
                 break;
         }
         return super.onTouchEvent(event);
     }
 
-    private void showClearIcon(boolean show) {
-        setCompoundDrawables(null, null, show ? mDrawable : null, null);
+    private void setClearIconVisible(boolean visible) {
+        setCompoundDrawables(getCompoundDrawables()[DRAWABLE_LEFT], getCompoundDrawables()[DRAWABLE_TOP],
+                visible ? mClearDrawable : null, getCompoundDrawables()[DRAWABLE_BOTTOM]);
     }
 }
